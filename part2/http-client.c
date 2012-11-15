6c21a2ae78bbe1260313e743f59d5530be8d1a35
Emily Schultz <ess2183@columbia.edu>
2012-11-15 14:15:42 -0500

    http-client.c removed echoing

--- part2/http-client.c
+++ part2/http-client.c
@@ -13,8 +13,6 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 
-#define RCVBUFSIZE 32   /* Size of receive buffer */
-
 // Error handling function
 void DieWithError(char *errorMessage)
 {
@@ -25,18 +23,12 @@ void DieWithError(char *errorMessage)
 int main(int argc, char *argv[])
 {
     int sock;                        /* Socket descriptor */
-    struct sockaddr_in echoServAddr; /* Echo server address */
-    unsigned short echoServPort;     /* Echo server port */
-    char *echoString;                /* String to send to echo server */
-    char echoBuffer[RCVBUFSIZE];     /* Buffer for echo string */
-    unsigned int echoStringLen;      /* Length of string to echo */
-    int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv() 
-                                        and total bytes read */
+    struct sockaddr_in servAddr;     /* server address */
+    unsigned short servPort;         /* server port */
 
     if (argc != 4)    /* Test for correct number of arguments */
     {
-       fprintf(stderr, "Usage: %s <Hostname> <Port> <URL>\n",
-               argv[0]);
+       fprintf(stderr, "Usage: %s <Hostname> <Port> <URL>\n", argv[0]);
        exit(1);
     }
     
@@ -44,14 +36,12 @@ int main(int argc, char *argv[])
     // convert hostname into an IP address
     struct hostent *he;
     if((he = gethostbyname(serverName)) == NULL)
-      {
 	DieWithError("gothostbyname failed");
-      }
     // server IP address
     char *servIP = inet_ntoa(*(struct in_addr *)he->h_addr);
 
     // Port Number
-    echoServPort = atoi(argv[2]);
+    servPort = atoi(argv[2]);
 
     // rest of the URL
     char *url = argv[3];
@@ -61,36 +51,16 @@ int main(int argc, char *argv[])
         DieWithError("socket() failed");
 
     /* Construct the server address structure */
-    memset(&echoServAddr, 0, sizeof(echoServAddr));     /* Zero out structure */
-    echoServAddr.sin_family      = AF_INET;             /* Internet address family */
-    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
-    echoServAddr.sin_port        = htons(echoServPort); /* Server port */
+    memset(&servAddr, 0, sizeof(servAddr));     /* Zero out structure */
+    servAddr.sin_family      = AF_INET;             /* Internet address family */
+    servAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
+    servAddr.sin_port        = htons(servPort); /* Server port */
 
-    /* Establish the connection to the echo server */
-    if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+    /* Establish the connection to the server */
+    if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
         DieWithError("connect() failed");
 
-    echoStringLen = strlen(echoString);          /* Determine input length */
-
-    /* Send the string to the server */
-    if (send(sock, echoString, echoStringLen, 0) != echoStringLen)
-        DieWithError("send() sent a different number of bytes than expected");
-
-    /* Receive the same string back from the server */
-    totalBytesRcvd = 0;
-    printf("Received: ");                /* Setup to print the echoed string */
-    while (totalBytesRcvd < echoStringLen)
-    {
-        /* Receive up to the buffer size (minus 1 to leave space for
-           a null terminator) bytes from the sender */
-        if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0)
-            DieWithError("recv() failed or connection closed prematurely");
-        totalBytesRcvd += bytesRcvd;   /* Keep tally of total bytes */
-        echoBuffer[bytesRcvd] = '\0';  /* Terminate the string! */
-        printf("%s", echoBuffer);      /* Print the echo buffer */
-    }
-
-    printf("\n");    /* Print a final linefeed */
+    printf("HI");
 
     close(sock);
     exit(0);
